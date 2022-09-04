4002451a003dfb7c7b3988f758ebcb5875e47f58
peterpa-web <peterpa@live.de>
2022-09-04 21:13:44 +0200

    Added ReadStringA

--- StringUtil.cpp
+++ StringUtil.cpp
@@ -18,3 +18,41 @@ CStringW CStringUtil::FromUtf8(LPCSTR pszUtf8)
 	return str;
 }
 
+BOOL CStringUtil::ReadStringA(CStdioFile& f, CStringA& rString)
+{
+	rString = "";    // empty string without deallocating
+	const int nMaxSize = 128;
+	LPSTR lpsz = rString.GetBuffer(nMaxSize);
+	LPSTR lpszResult;
+	int nLen = 0;
+	for (;;)
+	{
+		lpszResult = fgets(lpsz, nMaxSize + 1, f.m_pStream);
+		rString.ReleaseBuffer();
+
+		// handle error/eof case
+		if (lpszResult == NULL && !feof(f.m_pStream))
+		{
+			Afx_clearerr_s(f.m_pStream);
+			AfxThrowFileException(CFileException::genericException, _doserrno,
+				f.GetFileName());
+		}
+
+		// if string is read completely or EOF
+		if (lpszResult == NULL ||
+			(nLen = AtlStrLen(lpsz)) < nMaxSize ||
+			lpsz[nLen - 1] == '\n')
+			break;
+
+		nLen = rString.GetLength();
+		lpsz = rString.GetBuffer(nMaxSize + nLen) + nLen;
+	}
+
+	// remove '\n' from end of string if present
+	lpsz = rString.GetBuffer(0);
+	nLen = rString.GetLength();
+	if (nLen != 0 && lpsz[nLen - 1] == '\n')
+		rString.GetBufferSetLength(nLen - 1);
+
+	return nLen != 0;
+}
--- StringUtil.h
+++ StringUtil.h
@@ -3,5 +3,6 @@ class CStringUtil
 {
 public:
 	static CStringW FromUtf8(LPCSTR pszUtf8);
+	static BOOL ReadStringA(CStdioFile& f, CStringA& rString);
 };
 
