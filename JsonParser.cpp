9048455c696586585e5e689923b32bc84a137a62
peterpa-web <peterpa@live.de>
2022-11-15 18:33:53 +0100

    new AreaGen, Json parser with thread

--- JsonParser.cpp
+++ JsonParser.cpp
@@ -1,29 +1,166 @@
 #include "stdafx.h"
 #include "JsonParser.h"
 
+CJsonParser::~CJsonParser()
+{
+    if (m_pThread != nullptr)
+    {
+        m_bCanceled = true;
+        FinThread();
+    }
+}
+
 bool CJsonParser::Parse(const CStringA strInput)
 {
     m_nStart = 0;
+    m_nDone = 0;
     m_strData = strInput;
 
     return ParseObject("");
 }
 
-bool CJsonParser::ParseObject(CStringA strContext)
+bool CJsonParser::PrepThread()
 {
-    int nStart = m_nStart;
-    char c = m_strData[nStart];
-    if (c != '{')
+    TRACE0("PrepThread\n");
+    m_pThread = AfxBeginThread(Run, this, THREAD_PRIORITY_NORMAL, 0, CREATE_SUSPENDED);
+    if (m_pThread == nullptr)
+        return false;
+    m_evEmpty.ResetEvent();
+    m_evNewData.ResetEvent();
+    m_pThread->m_bAutoDelete = FALSE;
+    m_pThread->ResumeThread();
+    return true;
+}
+
+UINT __cdecl CJsonParser::Run(LPVOID pParam)
+{
+    CJsonParser* pThis = (CJsonParser*)pParam;
+    return pThis->RunThis();
+}
+
+UINT CJsonParser::RunThis()
+{
+    m_nStart = 0;
+    m_nDone = 0;
+    int nStart = 0;
+    m_bCanceled = false;
+
+//    m_evEmpty.SetEvent();
+    if (!WaitThread(nStart, _T("Run")))
+        return 1;
+    bool bRet = ParseObject("");
+    if (!bRet)
     {
-        TRACE3("ParseObject from %d: Bad start char %c at %d\n", c, nStart, m_nStart);
+        m_bCanceled = true;
+        m_evEmpty.SetEvent();   // allow ContThread detecting cancelation
+    }
+    return bRet;
+}
+
+bool CJsonParser::WaitThread(int nStart, LPCTSTR pszMsg)
+{
+    if (m_pThread != nullptr)
+    {
+//        TRACE1("WaitThread %s begin\n", pszMsg);
+        m_evEmpty.SetEvent();
+        if (m_bCanceled)
+        {
+            TRACE0("WaitThread canceled\n");
+            return false;
+        }
+        // wait for ContThread providing new input data
+        DWORD dwRes = WaitForSingleObject(m_evNewData.m_hObject, m_dwWaitEvent);
+        if (dwRes == WAIT_TIMEOUT)
+        {
+            TRACE3("%s Wait Timeout from %d: EOD at %d\n", pszMsg, nStart, m_nDone + m_nStart);
+            return false;
+        }
+        if (dwRes == WAIT_FAILED)
+        {
+            TRACE3("%s Wait failed from %d: EOD at %d\n", pszMsg, nStart, m_nDone + m_nStart);
+            TRACE1("  Err=%d\n", GetLastError());
+            return false;
+        }
+ //       TRACE1("WaitThread %s end\n", pszMsg);
+        m_evNewData.ResetEvent();
+        return true;
+    }
+    TRACE3("%s from %d: EOD at %d\n", pszMsg, nStart, m_nDone + m_nStart);
+    return false;
+}
+
+bool CJsonParser::ContThread(const CStringA& strNextInput)
+{
+    ASSERT(m_pThread != nullptr);
+    // wait for WaitThread where all input data are processed
+    DWORD dwRes = WaitForSingleObject(m_evEmpty.m_hObject, m_dwWaitEvent);
+    if (dwRes == WAIT_TIMEOUT)
+    {
+        TRACE1("ContThread Timeout at %d\n", m_nDone + m_nStart);
         return false;
     }
-    TRACE2("ParseObject %d %S\n", nStart, strContext);
+    if (dwRes == WAIT_FAILED)
+    {
+        TRACE1("ContThread failed at %d\n", m_nDone + m_nStart);
+        TRACE1("  Err=%d\n", GetLastError());
+        return false;
+    }
+ //   TRACE1("ContThread >%S<\n", strNextInput);
+    m_evEmpty.ResetEvent();
+    if (m_bCanceled)
+    {
+        TRACE0("ContThread canceled\n");
+        return false;
+    }
+    m_nDone += m_strData.GetLength();
+    m_nStart = 0;
+    m_strData = strNextInput;
+    SkipSpaces();
+    m_evNewData.SetEvent();
+    return true;
+}
+
+bool CJsonParser::FinThread()
+{
+    TRACE0("FinThread begin\n");
+    DWORD dwRes = WaitForSingleObject(m_pThread->m_hThread, m_dwWaitEvent);
+    if (dwRes == WAIT_TIMEOUT)
+    {
+        TRACE1("FinThread Timeout at %d\n", m_nDone + m_nStart);
+        return false;
+    }
+    if (dwRes == WAIT_FAILED)
+    {
+        TRACE1("FinThread failed at %d\n", m_nDone + m_nStart);
+        TRACE1("  Err=%d\n", GetLastError());
+        return false;
+    }
+
+    // get return value of the thread (your actual question)
+    DWORD dwResult;
+    if (!GetExitCodeThread(m_pThread->m_hThread, &dwResult))
+        return false;
+    m_pThread->Delete();
+    m_pThread = nullptr;
+    TRACE0("FinThread end\n");
+    return dwRes == 0;
+}
+
+bool CJsonParser::ParseObject(CStringA strContext)
+{
+    int nStart = m_nStart + m_nDone;
+    char c = m_strData[m_nStart];
+    if (c != '{')
+    {
+        TRACE3("ParseObject from %d: Bad start char %C at %d\n", c, nStart, m_nDone + m_nStart);
+        return false;
+    }
+//    TRACE2("ParseObject %d %S\n", nStart, strContext);
     do {
-        if (++m_nStart > m_strData.GetLength())
+        if (++m_nStart >= m_strData.GetLength())
         {
-            TRACE2("ParseObject from %d: EOD at %d\n", nStart, m_nStart);
-            return false;
+            if (!WaitThread(nStart, _T("ParseObject_A")))
+                return false;
         }
         SkipSpaces();
         if (!ParsePair(strContext))
@@ -34,53 +171,82 @@ bool CJsonParser::ParseObject(CStringA strContext)
     SkipSpaces();
     if (c != '}')
     {
-        TRACE3("ParseObject from %d: Bad end char %C at %d\n", c, nStart, m_nStart);
+        TRACE3("ParseObject from %d: Bad end char %C at %d\n", c, nStart, m_nDone + m_nStart);
         return false;
     }
-    if (++m_nStart > m_strData.GetLength())
+    if (nStart == 0)
     {
-        TRACE2("ParseObject from %d: EOD at %d\n", nStart, m_nStart);
-        return false;
+        TRACE2("ParseObject from %d: Finished at %d\n", nStart, m_nDone + m_nStart);
+        return true;
     }
-    TRACE2("ParseObject from %d: done at %d\n", nStart, m_nStart);
+
+    if (++m_nStart >= m_strData.GetLength())
+    {
+        if (!WaitThread(nStart, _T("ParseObject_B")))
+            return false;
+    }
+//    TRACE2("ParseObject from %d: done at %d\n", nStart, m_nDone + m_nStart);
     return true;
 }
 
 bool CJsonParser::ParseArray(CStringA strContext)
 {
-    int nStart = m_nStart;
-    char c = m_strData[nStart];
+    int nStart = m_nDone + m_nStart;
+    char c = m_strData[m_nStart];
     if (c != '[')
         return false;   // bad start char
-    TRACE2("ParseArray %d %S\n", nStart, strContext);
+//    TRACE2("ParseArray %d %S\n", nStart, strContext);
     SkipSpaces();
     int nIndex = 0;
     do {
         CStringA strIndex;
-        strIndex.Format("[%d]", nIndex++);
-        if (++m_nStart > m_strData.GetLength())
+        strIndex.Format("[%d]", nIndex);
+        if (++m_nStart >= m_strData.GetLength())
         {
-            TRACE2("ParseArray from %d: EOD at %d\n", nStart, m_nStart);
-            return false;
+            if (!WaitThread(nStart, _T("ParseArray_A")))
+                return false;
         }
         SkipSpaces();
-        if (!ParseObject(strContext + strIndex))
-            return false;
+        CStringA strValue;
+        c = m_strData[m_nStart];
+        if (c == '{')
+        {
+            if (!ParseObject(strContext + strIndex))
+                return false;
+        }
+        else if (c == '[')
+        {
+            if (!ParseArray(strContext + strIndex))
+                return false;
+        }
+        else if (c == '"')
+        {
+            if (!ParseString(strValue))
+                return false;
+            StoreValue(strContext, nIndex, strValue);
+        }
+        else
+        {
+            if (!ParseValue(strValue))
+                return false;
+            StoreValue(strContext, nIndex, strValue);
+        }
+        ++nIndex;
         SkipSpaces();
         c = m_strData[m_nStart];
     } while (c == ',');
     SkipSpaces();
     if (c != ']')
     {
-        TRACE3("ParseArray from %d: Bad end char %C at %d\n", c, nStart, m_nStart);
+        TRACE3("ParseArray from %d: Bad end char %C at %d\n", c, nStart, m_nDone + m_nStart);
         return false;
     }
-    if (++m_nStart > m_strData.GetLength())
+    if (++m_nStart >= m_strData.GetLength())
     {
-        TRACE2("ParseArray from %d: EOD at %d\n", nStart, m_nStart);
-        return false;
+        if (!WaitThread(nStart, _T("ParseArray_B")))
+            return false;
     }
-    TRACE2("ParseArray from %d: done at %d\n", nStart, m_nStart);
+ //   TRACE2("ParseArray from %d: done at %d\n", nStart, m_nDone + m_nStart);
     return true;
 }
 
@@ -98,13 +264,13 @@ bool CJsonParser::ParsePair(CStringA strContext)
     char c = m_strData[m_nStart];
     if (c != ':')
     {
-        TRACE3("ParsePair from %d: Bad separator char %C at %d\n", c, nStart, m_nStart);
+        TRACE3("ParsePair from %d: Bad separator char %C at %d\n", c, nStart, m_nDone + m_nStart);
         return false;
     }
     if (++m_nStart > m_strData.GetLength())
     {
-        TRACE2("ParsePair from %d: EOD at %d\n", nStart, m_nStart);
-        return false;
+        if (!WaitThread(nStart, _T("ParsePair_A")))
+            return false;
     }
     SkipSpaces();
     c = m_strData[m_nStart];
@@ -116,11 +282,13 @@ bool CJsonParser::ParsePair(CStringA strContext)
     {
         if (!ParseString(strValue))
             return false;
+ //       StoreValue(strContext, -1,  strValue);
     }
     else
     {
         if (!ParseValue(strValue))
             return false;
+//        StoreValue(strContext, -1, strValue);
     }
 //    TRACE3("StorePair %d %S %S\n", nStart, strKey, strValue);
     StorePair(strContext, strKey, strValue);
@@ -133,7 +301,7 @@ bool CJsonParser::ParseString(CStringA& strResult)
     char c = m_strData[nStart];
     if (c != '"')
     {
-        TRACE3("ParseString from %d: Bad start char %C at %d\n", c, nStart, m_nStart);
+        TRACE3("ParseString from %d: Bad start char %C at %d\n", c, nStart, m_nDone + m_nStart);
         return false;
     }
     int p = nStart;
@@ -141,16 +309,17 @@ bool CJsonParser::ParseString(CStringA& strResult)
         p = m_strData.Find('"', p + 1);
         if (p < 0)
         {
-            TRACE2("ParseString from %d: EOD at %d\n", nStart, m_nStart);
-            return false;
+            if (!WaitThread(nStart, _T("ParseString_A")))
+                return false;
+            p = 0;
         }
     } while (m_strData[p - 1] == '\\');
     strResult = m_strData.Mid(nStart + 1, p - nStart - 1);
     m_nStart = p + 1;
     if (m_nStart > m_strData.GetLength())
     {
-        TRACE2("ParseString from %d: EOD at %d\n", nStart, m_nStart);
-        return false;
+        if (!WaitThread(nStart, _T("ParseString_B")))
+            return false;
     }
     return true;
 }
@@ -159,12 +328,12 @@ bool CJsonParser::ParseValue(CStringA& strValue)
 {
     int nStart = m_nStart;
     char c = m_strData[m_nStart];
-    while (c != ',' && c != '}' && c != ' ' && c != '\t' && c != '\r' && c != '\n')
+    while (c != ',' && c != ']' && c != '}' && c != ' ' && c != '\t' && c != '\r' && c != '\n')
     {
         if (++m_nStart > m_strData.GetLength())
         {
-            TRACE2("ParseValue from %d: EOD at %d\n", nStart, m_nStart);
-            return false;
+            if (!WaitThread(nStart, _T("ParseValue_A")))
+                return false;
         }
         c = m_strData[m_nStart];
     }
--- JsonParser.h
+++ JsonParser.h
@@ -1,14 +1,33 @@
 #pragma once
+#include "afxmt.h"
+
 class CJsonParser
 {
 public:
-	virtual bool Parse(CStringA strInput);
+	~CJsonParser();
+	bool Parse(CStringA strInput);
 
 protected:
 	virtual void StorePair(const CStringA& strContext, const CStringA& strKey, const CStringA& strValue) {}
+	virtual void StoreValue(const CStringA& strContext, int nIndex, const CStringA& strValue) {}
+	bool PrepThread();
+	bool ContThread(const CStringA& strNextInput);
+	bool FinThread();		// wait for thread result
+
+private:
+	bool WaitThread(int nStart, LPCTSTR pszMsg);	// wait for ContThread or EOD-Msg
 
 	CStringA m_strData;
-	int m_nStart = 0;
+	int m_nStart = 0;	// index inside m_strData
+	int m_nDone = 0;	// size of already processed data
+	CWinThread* m_pThread = nullptr;
+	DWORD m_dwWaitEvent = INFINITE;
+	CEvent m_evNewData;	// fired by ContThread
+	CEvent m_evEmpty;	// fired by WaitThread
+	bool m_bCanceled = false;
+
+	UINT static __cdecl Run(LPVOID pParam);
+	UINT RunThis();
 
 	bool ParseObject(CStringA strContext);
 	bool ParseArray(CStringA strContext);
